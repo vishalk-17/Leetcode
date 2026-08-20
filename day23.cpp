9e2a4c45f9feaa8a74f2af4379099364990e251d
vishalk-17 <vk0262401@gmail.com>
2026-08-20 23:50:21 +0530

    leet code 3039 soluiton

---
+++ day23.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+
+        vector<int> first;
+        vector<int> second;
+
+        first.push_back(nums[0]);
+        second.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+
+            if (first.back() > second.back()) {
+                first.push_back(nums[i]);
+            } 
+            else {
+                second.push_back(nums[i]);
+            }
+        }
+
+        // Combine both arrays
+        first.insert(first.end(), second.begin(), second.end());
+
+        return first;
+    }
+};
