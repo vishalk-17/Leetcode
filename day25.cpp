a85f68b38fed1a0c82ae2bae089754775c47d222
vishalk-17 <vk0262401@gmail.com>
2026-08-22 22:48:26 +0530

    leet code3622

---
+++ day25.cpp
@@ -0,0 +1,75 @@
+/*3622. Check Divisibility by Digit Sum and Product
+
+You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
+
+The digit sum of n (the sum of its digits).
+
+The digit product of n (the product of its digits).
+
+Return true if n is divisible by this sum; otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: n = 99
+
+Output: true
+
+Explanation:
+
+Since 99 is divisible by the sum (9 + 9 = 18) plus product (9 * 9 = 81) of its digits (total 99), the output is true.
+
+Example 2:
+
+Input: n = 23
+
+Output: false
+
+Explanation:
+
+Since 23 is not divisible by the sum (2 + 3 = 5) plus product (2 * 3 = 6) of its digits (total 11), the output is false.
+
+ 
+
+Constraints:
+
+1 <= n <= 106*/
+
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int original = n;
+        int digitsum = 0;
+        int digitproduct = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+
+            digitsum += digit;
+            digitproduct *= digit;
+        }
+
+        return original % (digitsum + digitproduct) == 0;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (obj.checkDivisibility(n))
+        cout << "Divisible" << endl;
+    else
+        cout << "Not Divisible" << endl;
+
+    return 0;
+}
