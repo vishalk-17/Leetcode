a5cea39572949f113022e1f4639e7ec1265b8f43
vishalk-17 <vk0262401@gmail.com>
2026-08-25 23:33:39 +0530

    3718 leet code solution

---
+++ day28.cpp
@@ -0,0 +1,13 @@
+//3718. Smallest Missing Multiple of K
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int>st(nums.begin(),nums.end());
+        int multiple = k;
+        while(st.count(multiple)){
+            multiple += k;
+        }
+        return multiple;
+    }
+};
