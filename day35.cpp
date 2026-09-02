db55ada058044013223979b36cf16f90dfa7db9f
vishalk-17 <vk0262401@gmail.com>
2026-09-02 22:14:25 +0530

    3875 leet code solve

---
+++ day35.cpp
@@ -0,0 +1,24 @@
+// 3875. Construct Uniform Parity Array I
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an array nums1 of n distinct integers.
+
+// You want to construct another array nums2 of length n such that the elements in nums2 are either all odd or all even.
+
+// For each index i, you must choose exactly one of the following (in any order):
+
+// nums2[i] = nums1[i]
+// nums2[i] = nums1[i] - nums1[j], for an index j != i
+// Return true if it is possible to construct such an array, otherwise, return false.
+
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return 1;
+    }
+};
