ad2fc4f00627412956cce43fe5c74ae2ef1a5c7a
vishalk-17 <vk0262401@gmail.com>
2026-09-09 20:08:09 +0530

    3871. Count Commas in Range II

---
+++ day40.cpp
@@ -0,0 +1,27 @@
+
+//3871. Count Commas in Range II
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+
+        // Each range starts at a power of 1000.
+        long long start = 1000;
+        long long commas = 1;
+
+        while (start <= n) {
+            long long end = start * 1000 - 1;
+
+            // Avoid overflow for very large values
+            if (end < start || end > n)
+                end = n;
+
+            ans += (end - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return ans;
+    }
+};
