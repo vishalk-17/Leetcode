6599647184b98f9277276c968708174cc1672122
vishalk-17 <vk0262401@gmail.com>
2026-09-24 23:10:35 +0530

    /*3550. Smallest Index With Digit Sum Equal to Index

---
+++ day55.cpp
@@ -0,0 +1,105 @@
+/*3550. Smallest Index With Digit Sum Equal to Index
+
+You are given an integer array nums.
+
+Return the smallest index i such that the sum of the digits of nums[i] is equal to i.
+
+If no such index exists, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,2]
+
+Output: 2
+
+Explanation:
+
+For nums[2] = 2, the sum of digits is 2, which is equal to index i = 2. Thus, the output is 2.
+Example 2:
+
+Input: nums = [1,10,11]
+
+Output: 1
+
+Explanation:
+
+For nums[1] = 10, the sum of digits is 1 + 0 = 1, which is equal to index i = 1.
+For nums[2] = 11, the sum of digits is 1 + 1 = 2, which is equal to index i = 2.
+Since index 1 is the smallest, the output is 1.
+Example 3:
+
+Input: nums = [1,2,3]
+
+Output: -1
+
+Explanation:
+
+Since no index satisfies the condition, the output is -1.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 1000*/
+
+// /class Solution {
+// public:
+//     int digitsum(int n){
+//         int sum = 0;
+//         while(n>0){
+//             sum += n%10;
+//             n /= 10;
+//         }
+//         return sum;
+//     }
+//     int smallestIndex(vector<int>& nums) {
+//         for(int i=0;i<nums.size();i++){
+//             if(digitsum(nums[i])==i){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int digitSum(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    int smallestIndex(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (digitSum(nums[i]) == i) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 10, 11};
+
+    int ans = obj.smallestIndex(nums);
+
+    cout << "Answer: " << ans << endl;
+
+    return 0;
+}
